10e9edd6f7d2d3b82f9222158f221b022c968db0
isaque-cwb <isaque.lourenco2@gmail.com>
2021-09-28 20:01:20 -0300

    telas criadas e class Alert

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -9,18 +9,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			mainScene = new Scene(parent);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sistema de Teste");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
